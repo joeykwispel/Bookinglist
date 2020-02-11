6f67bb8003f36d4aac4c936f2721a6e83fc999e5
unknown <joey.oosenbrug@gmail.com>
2020-02-11 17:36:49 +0100

    Last commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Books;
 use App\Form\BooksType;
 use App\Repository\BooksRepository;
+use App\Entity\Reviews;
+use App\Form\ReviewsType;
+use App\Repository\ReviewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,11 +17,12 @@ class HomeController extends AbstractController
      * @Route("/", name="Default")
      */
 
-    public function index(booksRepository $booksRepository)
+    public function index(booksRepository $booksRepository, ReviewsRepository $reviewsRepository)
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'Welcome',
             'books' => $booksRepository->findAll(),
+            'reviews' => $reviewsRepository->findAll(),
         ]);
     }
 }
